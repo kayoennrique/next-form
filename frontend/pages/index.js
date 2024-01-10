119cabe0056588d133be471ade64c5b5de480b81
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-09 21:00:05 -0300

    Starting the project

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,39 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
 export default function HomeScreen() {
+  const router = useRouter();
+  const [values, setValues] = React.useState({
+    usuario: 'kayoennrique',
+    senha: 'safepassword'
+  });
+
+  function handleChange(event) {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    setValues((currentValues) => {
+      return {
+        ...currentValues,
+        [fieldName]: fieldValue,
+      }
+    })
+  }
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={(event) => {
+        event.preventDefault();
+
+        // router.push('/auth-page-static')
+        router.push('/auth-page-ssr')
+      }}>
         <input
           placeholder="Usuário" name="usuario"
-          defaultValue="kayoennrique"
+          value={values.usuario} onChange={handleChange}
         />
         <input
           placeholder="Senha" name="senha" type="password"
-          defaultValue="safepassword"
+          value={values.senha} onChange={handleChange}
         />
         <div>
           <button>
